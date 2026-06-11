3637e3577ab6e629ca90df90d705bffdb0e918f2
Romashko_nastya <romashkonastya5@gmail.com>
2026-06-11 15:13:54 +0300

    add interfaces for API

---
+++ src/types/types.ts
@@ -0,0 +1,40 @@
+export interface MovieFromSearch {
+   Title: string;
+   Year: string;
+   imdbID: string;
+   Type: 'movie' | 'series' | 'episode';
+   Poster: string;
+}
+
+export interface OMDbSearchResponse {
+   Search?: MovieFromSearch[];
+   totalResults?: string;
+   Response: 'True' | 'False';
+   Error?: string;
+}
+
+export interface MovieDetail {
+   Title: string;
+   Year: string;
+   Rated: string;
+   Released: string;
+   Runtime: string;
+   Genre: string;
+   Director: string;
+   Writer: string;
+   Actors: string;
+   Plot: string;
+   Language: string;
+   Country: string;
+   Awards: string;
+   Poster: string;
+   imdbRating: string;
+   imdbID: string;
+   Type: 'movie' | 'series' | 'episode';
+   Response: 'True' | 'False';
+
+   BoxOffice?: string;
+   Season?: string;
+   Episode?: string;
+   seriesID?: string;
+}
